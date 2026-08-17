d20eaa7d5619956a0e771ed9e0d3d91d879bfb5b
zulaikhamir <Zulaikhamir786@gmail.com>
2026-08-17 14:59:28 +0530

    Add PostgreSQL, Flyway, and Actuator dependencies.

---
+++ src/main/java/com/ihya/api/IhyaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ihya.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhyaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhyaApiApplication.class, args);
+	}
+
+}
